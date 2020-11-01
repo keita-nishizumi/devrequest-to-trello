53863aafb805235160cdaed1c4b06ad953e9473c
Keita Mac <ktnshzm@gmail.com>
2020-11-01 21:34:15 +0900

    deleted Logger from form.js

--- form/form.js
+++ form/form.js
@@ -3,5 +3,4 @@ function setFirstQuestion() {
   const request_lists = SpreadSheetScript.setProperties();
   const values = JSON.parse(request_lists).map(list => list.board_name);
   first_question.asListItem().setChoiceValues(values);
-  Logger.log(values);
 }
