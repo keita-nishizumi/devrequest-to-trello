e27936ae31a168dd523eb1981717e56553a6e3e0
Keita Mac <ktnshzm@gmail.com>
2020-11-01 18:12:19 +0900

    created setProperties() func.

--- README.md
+++ README.md
@@ -4,10 +4,8 @@ This is an application that create new card when [V-factory development request 
 
 ## Installation
 
-Use npm for installation.
-
 ```bash
-npm install
+npm i @google/clasp -g
 ```
 
 Please create `.clasp.json` file and input your script ID.
@@ -21,6 +19,26 @@ Please create `.clasp.json` file and input your script ID.
 [V-factory開発依頼書](https://docs.google.com/forms/d/e/1FAIpQLScj3rb05Ze98xs1_LCxBjN1NSjBXXwUyVmbmdUrdHNmj8HU5A/viewform)のフォームが送信されると、Trello内の所定のボードの所定のリストにカードを追加する。
 
 TrelloのAPIキーやボード・リストIDはすべてプロジェクトのプロパティにセットしておき、そこから取得しています。
+
+## Maintenance（編集中）
+
+新しいボードが追加されたり、毎年4月にTrelloに新しいチームが作成されたら、以下の手順で「スクリプトのプロパティ」を設定してください。
+
+1. 以下のスプレッドシート（フォームの結果）にアクセス
+
+[V-factory開発依頼書（回答）](https://docs.google.com/spreadsheets/d/1cmBAS6tyYhg4_yOJ7P1OnxQ8qtl55YrM8lvikEPGgG8/edit#gid=1275307971)
+
+1. スクリプトのプロパティを開く
+
+ファイル > プロジェクトのプロパティ > スクリプトのプロパティ
+
+1. 管理者のユーザ情報（ユーザ名、APIキー、APIトークン）を設定する ※必ずチームに参加しているTrelloユーザで行ってください
+
+|プロパティ|値   |
+| ------ | --- |
+| user_name | Trelloのプロフィールから確認してください |
+| api_key   | [ここから取得](https://trello.com/app-key)       |
+| api_token | [ここから自動生成する](https://trello.com/app-key) |
 
 ## Contributing
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
--- app.js
+++ app.js
@@ -1,37 +1,61 @@
 //始めに、プロジェクトのプロパティを読み込む
-const propaties = PropertiesService.getScriptProperties();
+const properties = PropertiesService.getScriptProperties();
 
 // ユーザー名
-const user_name = propaties.getProperty('user_name');
+const user_name = properties.getProperty('user_name');
 
 // APIキーとトークン
-const api_key = propaties.getProperty('api_key');
-const api_token = propaties.getProperty('api_token');
+const api_key = properties.getProperty('api_key');
+const api_token = properties.getProperty('api_token');
 
-// ボードID
-const board_id = propaties.getProperty('board_id'); //83期Trello リファレンスボード
+const team_name = properties.getProperty('team_name');
+const request_list_name = properties.getProperty('request_list_name');
 
-// リストID
-const list_id = propaties.getProperty('list_id'); //[テスト] 開発依頼 ボード
+const request_lists = JSON.parse(setProperties());
+
+// 最初の質問の答え（開発を依頼するシステムの選択）が入っている列の番号
+const selected_system_no = 3;
 
 // カード名の列番号
 const title_column_no = 5;
 // カード詳細説明の列番号
 const description_column_no = 6;
 
-function getBoards() {
-  const url = 'https://api.trello.com/1/members/' + user_name + '/boards?key=' + api_key + '&token=' + api_token + '&fields=name';
-  Logger.log(UrlFetchApp.fetch(url, {'method':'get'}));
+function setProperties() {
+  const boards = getTeamBoards(team_name);
+  const request_lists_json = getRequestLists(boards, request_list_name);
+  const request_lists = JSON.stringify(request_lists_json);
+  properties.setProperty('request_lists', request_lists);
+  return request_lists;
 }
 
-function getlists() {
-  const url = "https://trello.com/1/boards/" + board_id + "/lists?key=" + api_key + "&token=" + api_token + "&fields=name";
-  Logger.log(UrlFetchApp.fetch(url, {'method':'get'}));
+// チーム名を渡すと、そのチームの全ボードIDを取得してくれる関数
+function getTeamBoards(team_name) {
+  const url = `https://api.trello.com/1/organizations/${team_name}/boards?key=${api_key}&token=${api_token}&fields=id,name`;
+  const response = UrlFetchApp.fetch(url, {'method':'get'});
+  const response_data = JSON.parse(response.getContentText());
+  return response_data;
 }
 
-function getLabels() {
-  const url = "https://trello.com/1/boards/" + board_id + "/labels?key=" + api_key + "&token=" + api_token + "&fields=name";
-  Logger.log(UrlFetchApp.fetch(url, {'method':'get'}));
+// ボードのリストとリスト名を渡すと、それぞれのボードの中で指定したリスト名をもつリストのIDを取得してくれる関数
+// 返り値は{ボード名：指定したリストのID}の配列になる
+function getRequestLists(boards, request_list_name) {
+  const retMap = boards.map(board => {
+    const url = `https://api.trello.com/1/boards/${board.id}/lists?key=${api_key}&token=${api_token}&filter=open&fields=id,name`
+    const response = UrlFetchApp.fetch(url, {'method':'get'});
+    const response_data = JSON.parse(response.getContentText());
+    // 名前にlist_nameが含まれているリストだけ抜き出します
+    const request_lists = response_data.filter(list => list.name.indexOf(request_list_name) >= 0 );
+    if (request_lists[0]) {
+      return {
+        'board_id': board.id,
+        'board_name': board.name,
+        'list_id': request_lists[0].id,
+        'list_name': request_lists[0].name
+      };
+    }
+  })
+  return retMap.filter(e => e);
 }
 
 function addTrelloCard() {
@@ -40,6 +64,12 @@ function addTrelloCard() {
   const sheet = ss.getActiveSheet();
   const last_row = sheet.getLastRow();
 
+  const selected_system = sheet.getRange(last_row, selected_system_no).getValue();
+  const list_id = request_lists.find(list => list.board_name == selected_system).list_id;
+  if (!list_id) {
+    sheet.getRange(last_row, 13).setValue('Could not fetch list_id.');
+    return null;
+  }
   const card_title = sheet.getRange(last_row, title_column_no).getValue();
   const card_description = sheet.getRange(last_row, description_column_no).getValue();
   const url = 'https://api.trello.com/1/cards/?key=' + api_key + '&token=' + api_token;
@@ -60,38 +90,3 @@ function addTrelloCard() {
   sheet.getRange(last_row, 13).setValue(response_data['shortUrl']);
 }
 
-function addAllTrelloCards() {
-
-  // 選択しているセルの開始行番号を取得
-  const ss = SpreadsheetApp.getActiveSpreadsheet();
-  const sheet = ss.getActiveSheet();
-  const upper_left_cell = sheet.getActiveCell();
-  const start_row = upper_left_cell.getRow();
-
-  // 選択しているセルの行数を取得
-  const range = SpreadsheetApp.getActiveRange();
-  const rows = range.getNumRows();
-
-  // カード作成
-  for (let i = 0; i < rows; i++) {
-    const row = start_row + i;
-    const card_title = sheet.getRange(row, title_column_no).getValue();
-    const card_description = sheet.getRange(row, description_column_no).getValue();
-    const url = 'https://api.trello.com/1/cards/?key=' + api_key + '&token=' + api_token;
-    const options = {
-      'method' : 'post',
-      'muteHttpExceptions' : true,
-      'payload' : {
-        'name'      : card_title,
-        'desc'      : card_description,
-        'due'       : '',
-        'idList'    : list_id,
-        // 'idLabels'  : 'ラベルを使用したい場合はgetLabelsを実行してよしなに',
-        'urlSource' : ''
-      }
-    }
-    const response = UrlFetchApp.fetch(url, options);
-    const response_data = JSON.parse(response.getContentText());
-    sheet.getRange(row, 13).setValue(response_data['shortUrl']);
-  }
-}
